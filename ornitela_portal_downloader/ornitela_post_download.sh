f0572415bdf10f124cb21975cdc11d99035eaca7
Dylan Winters <dylan.winters@oregonstate.edu>
2021-08-10 16:50:52 -0700

    re-organize, add processed .mat file upload

--- ornitela_portal_downloader/ornitela_post_download.sh
+++ ornitela_portal_downloader/ornitela_post_download.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+
+# Do MATLAB processing
+echo "Running MATLAB processing..."
+/usr/local/bin/matlab -nodisplay -r "run('/home/dw/projects/DASHCAMS/mfiles/run_after_download.m')" > /dev/null
+
+# Sync files to Box with rclone
+
 # Path to installed config file
 config=/usr/local/DASHCAMS/ornitela_config.yaml
 
@@ -13,5 +20,17 @@ rclone --config $rclone_conf sync "$local" "box:$remote"
 # Copy GPS
 local=$(yq -r .gps_local $config)
 remote=$(yq -r .gps_remote $config)
+echo "Syncing $local to box:$remote"
+rclone --config $rclone_conf sync "$local" "box:$remote"
+
+# Copy dives file
+local="/home/dw/projects/DASHCAMS/data/processed/ornitela_dives.csv"
+remote="DASHCAMS/data/Processed"
+echo "Syncing $local to box:$remote"
+rclone --config $rclone_conf copy "$local" "box:$remote"
+
+# Copy processed data files
+local="/home/dw/projects/DASHCAMS/data/processed/ornitela_bursts"
+remote="DASHCAMS/data/Processed/ornitela_bursts"
 echo "Syncing $local to box:$remote"
 rclone --config $rclone_conf sync "$local" "box:$remote"
