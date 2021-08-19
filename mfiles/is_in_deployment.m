ffd6631c1eb37ff44b8cdd19468f6d96081706f8
Dylan Winters <dylan.winters@oregonstate.edu>
2021-08-19 09:37:29 -0700

    manually add LI deployments

--- mfiles/is_in_deployment.m
+++ mfiles/is_in_deployment.m
@@ -29,6 +29,11 @@ if nargin > 2
         locs = {'Middleton Island AK'};
       case 'UAE'
         locs = {'Butina Island UAE', 'Siniya Island UAE'};
+      case 'LI' % FIXME: Manually added these
+        yn = id==202074 & dn > datenum([2021 05 21 15 58 00]) | ...
+             id==203022 & dn > datenum([2021 05 21 15 55 00]) | ...
+             id==203037 & dn > datenum([2021 05 31 12 27 00]);
+        return
     end
     fdata = fdata(ismember(fdata.Capture_Site,locs),:);
 end
@@ -58,4 +63,12 @@ for i = 1:length(uid)
     for ii = 1:length(dn1)
         yn(m) = yn(m) | (dn(m) >= datenum(dn0(ii)) & dn(m) <= datenum(dn1(ii)));
     end
+end
+
+% FIXME: Manually added LI deployments
+if nargin < 2
+    yn = yn | ...
+         id==202074 & dn > datenum([2021 05 21 15 58 00]) | ...
+         id==203022 & dn > datenum([2021 05 21 15 55 00]) | ...
+         id==203037 & dn > datenum([2021 05 31 12 27 00]);
 end
