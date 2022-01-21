802fccdd4093afec4269149579e448d284456868
Dylan Winters <dylan.winters@oregonstate.edu>
2022-01-21 09:31:43 -0800

    add service to periodically download deployment metadata from Box

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ sudo cp ornitela_portal_downloader/services/dashcams-ornitela-download-prev-mont
 sudo cp ornitela_portal_downloader/services/dashcams-ornitela-download-prev-month.timer   /etc/systemd/system
 sudo cp ornitela_ftp_copy/dashcams-ornitela-ftp-copy.service   /etc/systemd/system
 sudo cp ornitela_ftp_copy/dashcams-ornitela-ftp-copy.timer     /etc/systemd/system
-
+sudo cp services/dashcams-download-field-data.service /etc/systemd/system
+sudo cp services/dashcams-download-field-data.timer   /etc/systemd/system
 
 # Copy configuration and executables
 sudo mkdir -p /usr/local/DASHCAMS
@@ -16,12 +17,15 @@ sudo cp ornitela_portal_downloader/download_month.py  /usr/local/DASHCAMS
 sudo cp ornitela_portal_downloader/ornitela_post_download.sh /usr/local/DASHCAMS
 sudo cp -r ornitela_portal_downloader/classes         /usr/local/DASHCAMS
 sudo cp ornitela_ftp_copy/ornitela_ftp_copy.sh        /usr/local/DASHCAMS
+sudo cp services/dashcams-download-field-data.sh      /usr/local/DASHCAMS
 
 # Enable timers
 sudo systemctl enable dashcams-ornitela-download-month.timer
 sudo systemctl enable dashcams-ornitela-download-prev-month.timer
 sudo systemctl enable dashcams-ornitela-ftp-copy.timer
+sudo systemctl enable dashcams-download-field-data.timer
 
 sudo systemctl restart dashcams-ornitela-download-month.timer
 sudo systemctl restart dashcams-ornitela-download-prev-month.timer
 sudo systemctl restart dashcams-ornitela-ftp-copy.timer
+sudo systemctl restart dashcams-download-field-data.timer
---
+++ services/dashcams-download-field-data.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Path to installed config file
+config=/usr/local/DASHCAMS/ornitela_config.yaml
+
+# Copy deployment field data csv
+local=$(yq -r .field_data_local $config)
+remote=$(yq -r .field_data_remote $config)
+rclone_conf=$(yq -r .rclone_config $config)
+echo "Copying $remote to $local"
+rclone --config $rclone_conf copy "box:$remote" "$local"
