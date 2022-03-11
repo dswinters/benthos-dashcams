c564b00739f16f41b388c9c7cedbce8fa92059a9
Dylan Winters <dylan.winters@oregonstate.edu>
2022-03-11 15:01:42 -0800

    Fix services, pull from repo before running

--- services/dashcams-run-R-analysis.sh
+++ services/dashcams-run-R-analysis.sh
@@ -9,8 +9,12 @@ dir_in=/home/DASHCAMS/data_raw/ornitela_ftp/
 dir_out=/home/DASHCAMS/data_processed/zTagStatus/
 depdat=/home/DASHCAMS/data_raw/metadata/DASHCAMS_Deployment_Field_Data.csv
 
-Rscript --vanilla /home/DASHCAMS/git/CormOcean/TagStatus_1wk.R "$dir_in" "$depdat" "$dir_out"
-Rscript --vanilla /home/DASHCAMS/git/CormOcean/TagStatus_24hr.R "$dir_in" "$depdat" "$dir_out"
+# Update Rachael's code from git
+repo=/home/DASHCAMS/git/CormOcean/
+git -C $repo pull
+
+Rscript --vanilla $repo/TagStatus_1wk.R "$dir_in" "$depdat" "$dir_out"
+Rscript --vanilla $repo/TagStatus_24hr.R "$dir_in" "$depdat" "$dir_out"
 
 # Copy data
 local=/home/DASHCAMS/data_processed/zTagStatus
