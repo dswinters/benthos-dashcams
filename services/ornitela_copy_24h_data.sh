0d4bb6f8441cde207f11b807eeab2110045457c2
Dylan Winters <dylan.winters@oregonstate.edu>
2022-03-11 14:46:50 -0800

    re-organize R analysis code

--- services/ornitela_copy_24h_data.sh
+++ services/ornitela_copy_24h_data.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO: We no longer need the last 24hrs of Ornitela data; since we are now
-# using the FTP data as a source.
-
 # Get rclone config info from our config file
 config=/usr/local/DASHCAMS/ornitela_config.yaml
 rclone_conf=$(yq -r .rclone_config $config)
@@ -12,15 +9,8 @@ dir_in=/home/DASHCAMS/data_raw/ornitela_ftp/
 dir_out=/home/DASHCAMS/data_processed/zTagStatus/
 depdat=/home/DASHCAMS/data_raw/metadata/DASHCAMS_Deployment_Field_Data.csv
 
-Rscript --vanilla /home/dw/projects/DASHCAMS/repos/CormOcean/TagStatus_1wk.R "$dir_in" "$depdat" "$dir_out"
-# Rscript --vanilla /home/dw/projects/DASHCAMS/repos/CormOcean/TagStatus_24hr.R "$dir_in" "$depdat" "$dir_out"
-
-# Copy data
-local=/home/DASHCAMS/data_raw/ornitela_last24h
-remote=DASHCAMS/data/ornitela_last24h
-
-echo "Syncing $local to box:$remote"
-rclone --config $rclone_conf sync "$local" "box:$remote"
+Rscript --vanilla /home/DASHCAMS/git/CormOcean/TagStatus_1wk.R "$dir_in" "$depdat" "$dir_out"
+Rscript --vanilla /home/DASHCAMS/git/CormOcean/TagStatus_24hr.R "$dir_in" "$depdat" "$dir_out"
 
 # Copy data
 local=/home/DASHCAMS/data_processed/zTagStatus
