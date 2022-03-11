a787476f816b339aef8f4307da218f9e67f09767
Dylan Winters <dylan.winters@oregonstate.edu>
2022-03-11 14:54:10 -0800

    rename R analysis code

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@
 # Install systemd services/timers
 sudo cp ornitela_portal_downloader/services/dashcams-ornitela-download-month.service      /etc/systemd/system
 sudo cp ornitela_portal_downloader/services/dashcams-ornitela-download-month.timer        /etc/systemd/system
-sudo cp ornitela_portal_downloader/services/dashcams-ornitela-download-24h.service        /etc/systemd/system
-sudo cp ornitela_portal_downloader/services/dashcams-ornitela-download-24h.timer          /etc/systemd/system
 sudo cp ornitela_portal_downloader/services/dashcams-ornitela-download-prev-month.service /etc/systemd/system
 sudo cp ornitela_portal_downloader/services/dashcams-ornitela-download-prev-month.timer   /etc/systemd/system
 sudo cp ornitela_ftp_copy/dashcams-ornitela-ftp-copy.service                              /etc/systemd/system
 sudo cp ornitela_ftp_copy/dashcams-ornitela-ftp-copy.timer                                /etc/systemd/system
 sudo cp services/dashcams-download-field-data.service                                     /etc/systemd/system
 sudo cp services/dashcams-download-field-data.timer                                       /etc/systemd/system
+sudo cp services/dashcams-run-R-analysis.service                                          /etc/systemd/system
+sudo cp services/dashcams-run-R-analysis.timer                                            /etc/systemd/system
+
 
 # Copy configuration and executables
 sudo mkdir -p                                                /usr/local/DASHCAMS
@@ -18,20 +19,21 @@ sudo cp config.yaml                                          /usr/local/DASHCAMS
 sudo cp ornitela_portal_downloader/download_month.py         /usr/local/DASHCAMS
 sudo cp ornitela_portal_downloader/ornitela_post_download.sh /usr/local/DASHCAMS
 sudo cp ornitela_portal_downloader/download_last_24h.py      /usr/local/DASHCAMS
-sudo cp ornitela_portal_downloader/ornitela_copy_24h_data.sh /usr/local/DASHCAMS
 sudo cp -r ornitela_portal_downloader/classes                /usr/local/DASHCAMS
 sudo cp ornitela_ftp_copy/ornitela_ftp_copy.sh               /usr/local/DASHCAMS
 sudo cp services/dashcams-download-field-data.sh             /usr/local/DASHCAMS
+sudo cp services/dashcams-run-R-analysis.sh                  /usr/local/DASHCAMS
+
 
 # Enable timers
 sudo systemctl enable dashcams-ornitela-download-month.timer
-sudo systemctl enable dashcams-ornitela-download-24h.timer
 sudo systemctl enable dashcams-ornitela-download-prev-month.timer
 sudo systemctl enable dashcams-ornitela-ftp-copy.timer
 sudo systemctl enable dashcams-download-field-data.timer
+sudo systemctl enable dashcams-run-R-analysis.timer
 
 sudo systemctl restart dashcams-ornitela-download-month.timer
-sudo systemctl restart dashcams-ornitela-download-24h.timer
 sudo systemctl restart dashcams-ornitela-download-prev-month.timer
 sudo systemctl restart dashcams-ornitela-ftp-copy.timer
 sudo systemctl restart dashcams-download-field-data.timer
+sudo systemctl restart dashcams-run-R-analysis.timer
